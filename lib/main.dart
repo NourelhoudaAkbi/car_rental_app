f97eb42490df78c0c36a2b18d0fee524a439fe02
NourelhoudaAkbi <nourelhoudaakbi@gmail.com>
2024-10-14 10:06:28 +0100

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
-import 'package:car_rental_app/presentation/pages/car_details_page.dart';
-import 'package:car_rental_app/presentation/pages/car_list_screen.dart';
-import 'package:car_rental_app/presentation/pages/maps_page.dart';
+import 'package:car_rental_app/firebase_options.dart';
+import 'package:car_rental_app/injection_container.dart';
+import 'package:car_rental_app/presentation/bloc/car_bloc.dart';
+import 'package:car_rental_app/presentation/bloc/car_event.dart';
 import 'package:car_rental_app/presentation/pages/onboarding_page.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:car_rental_app/data/models/car.dart';
 
-import 'package:car_rental_app/presentation/pages/onboarding_page.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  initInjection();
   runApp(const MyApp());
 }
 
@@ -23,14 +23,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return BlocProvider(
+      create: (_) => getIt<CarBloc>()..add(LoadCars()),
+      child: MaterialApp(
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: OnboardingPage()
+        home: OnboardingPage(),
+      ),
     );
   }
 }
